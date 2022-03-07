e8585ce420be1069753ccc9590b0373f0cf69ed9
Serhii <venna2013@gmail.com>
2022-03-07 19:28:00 +0200

    deploy scripts ready

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,23 +3,42 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+
+//npx hardhat run --network rinkeby scripts/deploy.ts
+//npx hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"
+
 import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Hero = await ethers.getContractFactory("Hero");
+  const Factory = await ethers.getContractFactory("DungeonFactory");
+  const hero = await Hero.deploy();
+  const factory = await Factory.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await hero.deployed();
+  await factory.deployed();
 
-  await greeter.deployed();
+  console.log("Hero deployed to:", hero.address);
+  console.log("Factory deployed to:", factory.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Wait 60 seconds for network sync");
+  await new Promise((resolve) => setTimeout(resolve, 60000)); // pause for Etherscan update
+
+  try {
+    await hre.run("verify:verify", {
+      address: hero.address,
+      // constructorArguments: [],
+      contract: "contracts/Hero.sol:Hero",
+    });
+    await hre.run("verify:verify", {
+      address: factory.address,
+      // constructorArguments: [],
+      contract: "contracts/DungeonFactory.sol:DungeonFactory",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
